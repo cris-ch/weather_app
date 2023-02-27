cc829342b14c34095b411d54d28e5b4896baf803
Cristian <cristian.christensen@gmail.com>
2023-02-27 18:33:19 +0100

    Getting lat & lon from API

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoAPIoptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b51b07b534mshf4bbf6df6de8232p1271b0jsn5ae576e800f5",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,22 +1,42 @@
-import {useState} from 'react'
-import { AsyncPaginate } from 'react-select-async-paginate'
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoAPIoptions } from "../../api";
 
-const Search = ({onSearchChange}) => {
-  const [search, setSearch] = useState(null)
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=50000&namePrefix=${inputValue}`,
+      geoAPIoptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude}, ${city.longitude}`,
+              label: `${city.name}, ${city.region}, ${city.countryCode}`,
+            };
+          }),
+        };
+      });
+  };
 
   const handleOnChange = (searchData) => {
-    setSearch(searchData)
-    onSearchChange(searchData)
-  }
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
 
   return (
-    <AsyncPaginate 
+    <AsyncPaginate
       placeholder="Search for city"
-      debounceTimeout={600}
+      debounceTimeout={1000}
       value={search}
       onChange={handleOnChange}
+      loadOptions={loadOptions}
     />
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
